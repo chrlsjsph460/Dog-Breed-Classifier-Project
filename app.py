35a4973ea0045cbe302c27365928bc35a45f1406
Charles Joseph <chrlsjsph460@gmail.com>
2023-03-31 14:20:30 -0500

    Update app.py
    add new route.

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
      
 @app.route('/home')
+@app.route('/')
 def home():
     return render_template('index.html')
  
